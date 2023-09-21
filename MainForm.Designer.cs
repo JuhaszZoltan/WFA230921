c1e9aceb1dc59ffe9ef18e6bdd99b7a40f976110
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-09-21 14:51:46 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WFA230921
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lsbTodo = new System.Windows.Forms.ListBox();
+            this.lblInfo02 = new System.Windows.Forms.Label();
+            this.lblInfo01 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.chbImportant = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(87, 63);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(193, 32);
+            this.txtItem.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnAdd.Location = new System.Drawing.Point(36, 112);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(244, 36);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // lsbTodo
+            // 
+            this.lsbTodo.FormattingEnabled = true;
+            this.lsbTodo.ItemHeight = 25;
+            this.lsbTodo.Location = new System.Drawing.Point(297, 42);
+            this.lsbTodo.Name = "lsbTodo";
+            this.lsbTodo.Size = new System.Drawing.Size(281, 254);
+            this.lsbTodo.TabIndex = 2;
+            // 
+            // lblInfo02
+            // 
+            this.lblInfo02.AutoSize = true;
+            this.lblInfo02.Location = new System.Drawing.Point(269, 13);
+            this.lblInfo02.Name = "lblInfo02";
+            this.lblInfo02.Size = new System.Drawing.Size(99, 26);
+            this.lblInfo02.TabIndex = 3;
+            this.lblInfo02.Text = "Todo list:";
+            // 
+            // lblInfo01
+            // 
+            this.lblInfo01.AutoSize = true;
+            this.lblInfo01.Location = new System.Drawing.Point(82, 34);
+            this.lblInfo01.Name = "lblInfo01";
+            this.lblInfo01.Size = new System.Drawing.Size(106, 26);
+            this.lblInfo01.TabIndex = 4;
+            this.lblInfo01.Text = "new item:";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDelete.Location = new System.Drawing.Point(36, 154);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(244, 36);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // chbImportant
+            // 
+            this.chbImportant.AutoSize = true;
+            this.chbImportant.Location = new System.Drawing.Point(32, 65);
+            this.chbImportant.Name = "chbImportant";
+            this.chbImportant.Size = new System.Drawing.Size(49, 30);
+            this.chbImportant.TabIndex = 5;
+            this.chbImportant.Text = "!!!";
+            this.chbImportant.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 316);
+            this.Controls.Add(this.chbImportant);
+            this.Controls.Add(this.lblInfo01);
+            this.Controls.Add(this.lblInfo02);
+            this.Controls.Add(this.lsbTodo);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtItem);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MainForm";
+            this.Text = "TODO App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lsbTodo;
+        private System.Windows.Forms.Label lblInfo02;
+        private System.Windows.Forms.Label lblInfo01;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.CheckBox chbImportant;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA230921
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.to_do_list;
+            btnAdd.Click += BtnAdd_Click;
+            btnDelete.Click += BtnDelete_Click;
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            lsbTodo.Items.Remove(lsbTodo.SelectedItem);
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtItem.Text))
+            {
+                if (chbImportant.Checked)
+                {
+                    txtItem.Text = $"[{txtItem.Text}]";
+                    chbImportant.Checked = false;
+                }
+                lsbTodo.Items.Add(txtItem.Text);
+                txtItem.Text = null;
+            }
+            else
+            {
+                MessageBox.Show(
+                    caption: "ERROR!",
+                    text: "U can not add nothing, u fckin idiot!",
+                    icon: MessageBoxIcon.Error,
+                    buttons: MessageBoxButtons.OK);
+            }
+
+        }
+    }
+}
